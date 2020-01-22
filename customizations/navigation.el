cb833902c88fc587745ada497358bc6a40eca53e
jonson <zhuimengshaonian04@gmail.com>
2020-01-22 23:21:28 +0800

    feat: add elfeed acewindow #q2ezaEce

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -32,6 +32,17 @@
 ;;      '(aw-leading-char-face
 ;;        ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
 ;;     ))
+;; (use-package ace-window
+;; :ensure t
+;; :init
+;; (progn
+;; (setq aw-scope 'global) ;; was frame
+;; (global-set-key (kbd "C-x O") 'other-frame)
+;;   (global-set-key [remap other-window] 'ace-window)
+;;   (custom-set-faces
+;;    '(aw-leading-char-face
+;;      ((t (:inherit ace-jump-face-foreground :height 3.0))))) 
+;;   ))
 
 ;;
 ;; ivy mode
